88174db5eb8a12620c16678acf2ddfffcfb2ce6b
Bobby <example@example.com>
2020-01-10 04:38:50 +0545

    Домашнее задание 1 выполнено

---
+++ domzadanie1/js.js
@@ -0,0 +1,19 @@
+let money = prompt('Ваш бюджет в месяц?', '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+    
+let appData = {
+    budget : money,
+    timeData : time,
+    expenses : {},
+    optionalExpenses  : {},
+    income : [],
+    savings : false
+};
+
+let a = prompt('Введите обязательную статью расходов в этом месяце');
+let b = prompt('Во сколько обойдется?');
+
+appData.expenses[0] = a;
+appData.expenses[1] = b;
+
+console.log(alert(appData.budget / 30));
