a54b4fe540068c7161288bbf470a894bbba18ed8
Bobby <example@example.com>
2020-01-12 20:30:54 +0545

    Домашнее задание 3 и 12 урок

---
+++ js/12.js
@@ -0,0 +1,19 @@
+// function showFirstMessage (text) {
+//     alert(text);
+// }
+// showFirstMessage('Go game!');
+
+function calc(a, b) {
+    return (a + b);
+}
+console.log(calc(4, 5));
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+let anotherNum = retVar();
+console.log(anotherNum);
+
+
+
